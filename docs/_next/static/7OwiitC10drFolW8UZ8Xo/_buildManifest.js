92c3148b3b0937d7df9846e59a875ed9382716b7
eddy <eddytech03@gmail.com>
2023-01-14 12:42:54 +0800

    remove

--- docs/_next/static/7OwiitC10drFolW8UZ8Xo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-88a8d142de1daaf2.js","static/chunks/pages/index-cab62541f20b784e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
