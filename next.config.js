cb6a03aacc427830f0f083b3ba4ca94f7f65950c
eddy <eddytech03@gmail.com>
2023-01-14 12:21:21 +0800

    Test accordingly

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+const repo = 'onesaujana'
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix,
+  basePath
 }
 
 module.exports = nextConfig
